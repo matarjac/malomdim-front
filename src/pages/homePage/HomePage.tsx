8bdd0430961e588f3a8988d6adea596e7203d226
Matar Jacob <matar.j@outlook.com>
2023-03-23 15:03:44 +0200

    commit progressbar animation

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Hero from "../../componnents/hero/Hero";
 import MainSubjectBox from "../../componnents/mainSubjectBox/MainSubjectBox";
 import "./HomePage.css";
 
 const HomePage: React.FC = () => {
-  return <div>
-<MainSubjectBox/>
-  </div>
+  return (
+    <div>
+      <MainSubjectBox />
+      <Hero />
+    </div>
+  )
 };
 
 export default HomePage;
