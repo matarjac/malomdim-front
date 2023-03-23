c7894c07398cd3998f29e16c7a76b52949472eba
Niv Rahamim <nivrm@gmail.com>
2023-03-23 15:01:57 +0200

    mainSub display

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -3,9 +3,11 @@ import MainSubjectBox from "../../componnents/mainSubjectBox/MainSubjectBox";
 import "./HomePage.css";
 
 const HomePage: React.FC = () => {
-  return <div>
-<MainSubjectBox/>
-  </div>
+  return (
+    <div>
+        <MainSubjectBox />
+    </div>
+  );
 };
 
 export default HomePage;
