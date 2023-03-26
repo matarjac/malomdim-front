38481818b0676d08dbe97b0d67e0f87c0d81c3b8
Matar Jacob <matar.j@outlook.com>
2023-03-26 10:59:21 +0300

    commit removal of previous design

--- src/componnents/frame/Frame.tsx
+++ src/componnents/frame/Frame.tsx
@@ -1,17 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
 import Footer from "..//footer/Footer";
-import Header from "..//header/header";
 
 
 
 const Frame = () => {
   return (
     <>
-      <Header />
       <Outlet />
       <Footer />
     </>
   );
 };
 
-export default Frame;
+export default Frame;
