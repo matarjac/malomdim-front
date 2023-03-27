63a9a40c4a9c8ecd5f5843917b6a108c9c55bdb1
Matar Jacob <matar.j@outlook.com>
2023-03-27 10:46:40 +0300

    Update LibrarySideMenu.tsx

--- src/componnents/library/librarySideMenu/LibrarySideMenu.tsx
+++ src/componnents/library/librarySideMenu/LibrarySideMenu.tsx
@@ -8,13 +8,12 @@ export const LibrarySideMenu: React.FC = () => {
 
     return (
         <LibrarySideMenuContainer>
-            {/* <div style={{ display: 'flex' }}> */}
             <div style={{ display: 'flex', padding: '10px, 8px', gap: '8px', alignItems: "center" }}>
                 <img src="./icons/sub-topics-icon.svg" alt="" style={{ padding: '4.75px' }} />
                 <GeneralSpan fontSize={18} fontWeight={500} >Sub Topics</GeneralSpan>
                 <AddButton style={{ alignSelf: 'flex-end' }} onClick={() => console.log("add")} />
             </div>
-            {/* </div> */}
+
 
             <SubTopicsListContainer>
                 <li style={{ all: 'unset' }}><SubTopicButton onClick={() => { setSelectedSubTopic('hooks') }} isSelected={selectedSubTopic == 'hooks'}>Hooks</SubTopicButton></li>
