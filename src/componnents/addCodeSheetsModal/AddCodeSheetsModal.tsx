cb6ff08efec9bf1da04cb484cc221962f46c8965
Niv Rahamim <nivrm@gmail.com>
2023-03-27 15:00:40 +0300

    Finish addSheetsmodal without connecting to the server

--- src/componnents/addCodeSheetsModal/AddCodeSheetsModal.tsx
+++ src/componnents/addCodeSheetsModal/AddCodeSheetsModal.tsx
@@ -3,7 +3,10 @@ import { useNavigate } from "react-router-dom";
 import InputFiled from "../inputFiled/inputFiled";
 import "./AddCodeSheetsModal.css";
 import "../../../src/pages/SingInPage/SingInPage.css";
-import MaterialsFilter from "../library/libraryContent/materialsFilter/MaterialsFilter";
+import {
+  FilterButton,
+  FiltersList,
+} from "../../StyledComponents/StyledLibrary";
 
 interface IAddCodeSheetProps {
   setShowCodeSheetModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,6 +19,12 @@ const AddCodeSheetsModal: React.FC<IAddCodeSheetProps> = ({
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [materialType, setMaterialType] = useState("text");
+
+  const handleMaterialTypeChange = (newMaterialType: string) => {
+    setMaterialType(newMaterialType);
+    setContent("");
+  };
 
   return (
     <div className="modal-overlay">
@@ -30,38 +39,80 @@ const AddCodeSheetsModal: React.FC<IAddCodeSheetProps> = ({
             alt="close codal icon"
           />
         </button>
-        <form className="add-code-sheets-form">
-          <div className="title-modal">Add Code Sheets.</div>
-
-          <div className="inputField">
-            <InputFiled
-              label={"Title"}
-              onChange={(value: any) => setTitle(value)}
-            />
-            <InputFiled
-              label={"Description"}
-              onChange={(value: any) => setDescription(value)}
-            />
-            <div className="content-input-box">
-              <textarea
-                name="Content"
-                className="content-input-text"
-                placeholder="Content"
-                onChange={(value: any) => setContent(value)}
-              />
-            </div>
+        <div className="add-code-sheets-form">
+        <div className="title-modal">Add Code Sheets.</div>
+          <div className="nav-bar-modal">
+            <FiltersList>
+              <FilterButton
+                isSelected={materialType === "text"}
+                onClick={() => handleMaterialTypeChange("text")}
+                style={{ width: "100px" }}
+              >
+                Text
+              </FilterButton>
+              <FilterButton
+                isSelected={materialType === "link"}
+                onClick={() => handleMaterialTypeChange("link")}
+                style={{ width: "100px" }}
+              >
+                Link
+              </FilterButton>
+              <FilterButton
+                isSelected={materialType === "code"}
+                onClick={() => handleMaterialTypeChange("code")}
+                style={{ width: "100px" }}
+              >
+                Code
+              </FilterButton>
+            </FiltersList>
           </div>
-          <button className="btu-save" type="submit">
-            Save
-          </button>
+          <form className="code-sheets-form">
+            <div className="inputField">
+              <InputFiled
+                label={"Title"}
+                onChange={(value: any) => setTitle(value)}
+              />
+              <InputFiled
+                label={"Description"}
+                onChange={(value: any) => setDescription(value)}
+              />
+              {materialType === "text" && (
+                <textarea
+                  name="Content"
+                  className="content-input-text"
+                  placeholder="Text"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+              )}
+              {materialType === "code" && (
+                <textarea
+                  name="Content"
+                  className="content-input-text"
+                  placeholder="Code"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+              )}
+              {materialType === "link" && (
+                <InputFiled
+                  label={"Link"}
+                  onChange={(value: any) => setDescription(value)}
+                />
+              )}
+            </div>
+            <button className="btu-save" type="submit">
+              Save
+            </button>
 
-          <button
-            className="btu-cancel"
-            onClick={() => setShowCodeSheetModal(false)}
-          >
-            Cancel
-          </button>
-        </form>
+            <button
+              className="btu-cancel"
+              onClick={() => setShowCodeSheetModal(false)}
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
