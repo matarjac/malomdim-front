0cee9d4fe60a7f1df21decbbc29e1e526a9a1e43
Matar Jacob <matar.j@outlook.com>
2023-03-28 17:45:32 +0300

    Added check on signIn, signUp inputs

--- src/pages/signInPage/SignInPage.tsx
+++ src/pages/signInPage/SignInPage.tsx
@@ -5,10 +5,19 @@ import { GeneralSpan } from "../../StyledComponents/StyledLibrary";
 
 const SignInPage: React.FC = () => {
   const navigate = useNavigate();
+
   const handleLogIn = (e: any) => {
     e.preventDefault();
-    sessionStorage.setItem('user', 'hello');
-    navigate("/");
+    const email: string = e.target[0].value;
+    const password: string = e.target[1].value;
+
+    if (!(email && password)) {
+      alert('Please fill all inputs.');
+    } else {
+      const credentials = { email, password }
+      sessionStorage.setItem('user', JSON.stringify(credentials));
+      navigate("/");
+    }
   }
   return (
     <SignInWindow>
@@ -16,59 +25,13 @@ const SignInPage: React.FC = () => {
       <LogInDetailsContainer>
         <GeneralSpan fontSize={28} fontWeight={600} style={{ padding: '10px' }}>LOGIN</GeneralSpan>
         <GeneralSpan fontSize={16} fontWeight={400} style={{ padding: '10px' }}>New? <StyledHref onClick={() => navigate("/sign-up")}>Create an account</StyledHref> </GeneralSpan>
-        <LogInForm>
+        <LogInForm onSubmit={handleLogIn}>
           <StyledInput type={'email'} placeholder={'Email Address'} />
           <StyledInput type={'password'} placeholder={'Password'} />
-          <StyledSubmitInput onClick={(e) => { handleLogIn(e) }} type={'submit'} value="Log In" />
+          <StyledSubmitInput type={'submit'} value="Log In" />
         </LogInForm>
       </LogInDetailsContainer>
     </SignInWindow>
   )
 }
-
 export default SignInPage;
-
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import InputFiled from "../../componnents/inputFiled/inputFiled";
-
-// import "./SingInPage.css";
-
-// const SingInPage = () => {
-//   const navigation: any = useNavigate();
-//   const [email, setEmail] = useState<string>("");
-//   const [password, setPassword] = useState<string>("");
-//   const CanLogIn: boolean = email.length > 5 && password.length > 8;
-
-//   return (
-//     <div className="main">
-//       <div className="singInCard">
-//         <div className="info">
-//           <span>SIGN IN</span>
-//           <p>To continue the order, please sign in</p>
-//         </div>
-//         <div className="inputField">
-//           <InputFiled
-//             label={"Email address"}
-//             onChange={(value: any) => setEmail(value)}
-//           />
-//           <InputFiled
-//             type="password"
-//             label={"Password"}
-//             onChange={(value: string) => setPassword(value)}
-//           />
-//         </div>
-//         <div className="logInForgetPassword">
-//           <button>login</button>
-//         </div>
-//         <div className="orDiv">
-//           <hr />
-//           <span>or</span>
-//           <hr />
-//         </div>
-//         <button onClick={() => navigation("/SingUpPage")}>sign up</button>
-//       </div>
-//     </div>
-//   );
-// };
-// export default SingInPage;
